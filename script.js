421c14dcbc1f05e532185d4b4fd31863b869503d
ahsan.btph123@gmail.com <ahsan.btph123@gmail.com>
2023-07-06 04:21:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const list = document.querySelectorAll(".list");
+
+list.forEach((item) =>
+  item.addEventListener("click", () => {
+    list.forEach((item) => item.classList.remove("active"));
+    item.classList.add("active");
+  })
+);
